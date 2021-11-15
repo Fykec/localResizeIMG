9f51011c3053dc8e209fb137fc7864a3eb09cf6f
yinjiaji <yinjiaji@egova.com.cn>
2021-11-15 15:37:48 +0800

    修复小米手机Mi 11 Lite上 useragent AppleWebKit 和数字之间 无 ‘/’ 斜线时的判断问题

--- lrz.bundle.js
+++ lrz.bundle.js
@@ -1230,7 +1230,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	        && !~navigator.userAgent.indexOf('Chrome');
 
 	    // QQ X5浏览器也有这个BUG
-	    return bCheck && navigator.userAgent.match(/AppleWebKit\/(\d+)/).pop() <= 534 || /MQQBrowser/g.test(navigator.userAgent);
+	    return (bCheck && navigator.userAgent.match(/AppleWebKit\/(\d+)/).pop() <= 534) ||
+	      (bCheck && navigator.userAgent.match(/AppleWebKit(\d+)/).pop() <= 534)  ||
+	      /MQQBrowser/g.test(navigator.userAgent);
 	}
 	var FormDataShim=(function(){
 	    var formDataShimNums = 0;
@@ -1238,13 +1240,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var
 	        // Store a reference to this
 	        o        = this,
-	    
+
 	        // Data to be sent
 	        parts = [],
-	    
+
 	        // Boundary parameter for separating the multipart values
 	        boundary = Array(21).join('-') + (+new Date() * (1e16 * Math.random())).toString(36),
-	    
+
 	        // Store the current XHR send method so we can safely override it
 	        oldSend  = XMLHttpRequest.prototype.send;
 	        this.getParts = function () {
@@ -1252,7 +1254,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        };
 	        this.append   = function (name, value, filename) {
 	            parts.push('--' + boundary + '\r\nContent-Disposition: form-data; name="' + name + '"');
-	    
+
 	            if (value instanceof Blob) {
 	                parts.push('; filename="' + (filename || 'blob') + '"\r\nContent-Type: ' + value.type + '\r\n\r\n');
 	                parts.push(value);
@@ -1262,19 +1264,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	            }
 	            parts.push('\r\n');
 	        };
-	    
+
 	        formDataShimNums++;
 	        XMLHttpRequest.prototype.send = function (val) {
 	            var fr,
 	                data,
 	                oXHR = this;
-	    
+
 	            if (val === o) {
 	                // Append the final boundary string
 	                parts.push('--' + boundary + '--\r\n');
 	                // Create the blob
 	                data = new BlobConstructor(parts);
-	    
+
 	                // Set up and read the blob into an array to be sent
 	                fr         = new FileReader();
 	                fr.onload  = function () {
@@ -1284,7 +1286,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    throw err;
 	                };
 	                fr.readAsArrayBuffer(data);
-	    
+
 	                // Set the multipart content type and boudary
 	                this.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 	                formDataShimNums--;
